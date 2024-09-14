768025060848f4c588539ea35ca9abd01a62070e
Ay-slim <ayooluwaadedipe@gmail.com>
2024-09-14 18:31:37 +0000

    Abstract error handling into an util file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,29 +1,22 @@
 import { Request, Response } from "express";
-import { RESERVED_ERROR_MESSAGES } from "../utils/constants";
 import services from "../services";
+import { error_handler } from "../utils/error_response";
+import { CACHE_EXPIRY } from "../utils/constants";
 
 export default async (req: Request, res: Response) => {
   try {
     const { city, date } = req.body;
-    const api_response = await services.weather_api_service(city, date);
+    const api_response = await services.weather_api_service(
+      city,
+      date,
+      CACHE_EXPIRY,
+    );
     res.status(200).json({
       data: api_response,
       success: true,
       message: "Api data fetched",
     });
   } catch (error) {
-    const error_id = new Date().getTime();
-    console.log({
-      error_id,
-      error: error?.stack || error,
-    });
-    res.status(error?.statusCode).json({
-      success: false,
-      message:
-        !error?.message || RESERVED_ERROR_MESSAGES.includes(error?.message)
-          ? "Something went wrong"
-          : error?.message,
-      error_id,
-    });
+    error_handler(error, res);
   }
 };
--- src/utils/error_response.ts
+++ src/utils/error_response.ts
@@ -1,4 +1,6 @@
-export default class ErrorResponse extends Error {
+import { Response } from "express";
+import { CUSTOM_ERROR_PREFIX } from "./constants";
+export class ErrorResponse extends Error {
   statusCode: number;
   stack;
   constructor(message: string, statusCode: number, stack: unknown) {
@@ -7,3 +9,18 @@ export default class ErrorResponse extends Error {
     this.stack = stack;
   }
 }
+
+export const error_handler = (error: ErrorResponse, res: Response) => {
+  const error_id = new Date().getTime();
+  console.log({
+    error_id,
+    error: error?.stack || error,
+  });
+  res.status(error?.statusCode || 500).json({
+    success: false,
+    message: error?.message?.startsWith(CUSTOM_ERROR_PREFIX)
+      ? error?.message
+      : "Something went wrong. We're working on it",
+    error_id,
+  });
+};
