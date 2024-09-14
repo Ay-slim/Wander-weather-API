6435d3900ccb5f1401b21ed32ea29294e8ae3247
Ay-slim <ayooluwaadedipe@gmail.com>
2024-09-14 10:18:39 +0000

    Call utils in weather service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,54 +1,7 @@
-import axios from "axios";
-import ErrorResponse from "../utils/ErrorResponse";
-import {
-  ValidatedApiResponseType,
-  WeatherApiResponseType,
-} from "../utils/types";
-
-export const farhenheitToCelsius = (fahrenheit: number): number => {
-  return ((fahrenheit - 32) * 5) / 9;
-};
-
-export const celsiusToFahrenheit = (celsius: number): number => {
-  return (celsius * 9) / 5 + 32;
-};
-
-const response_handler = (
-  api_response: WeatherApiResponseType,
-): ValidatedApiResponseType => {
-  const currentUnit = Object.keys(api_response)?.[0];
-  if (currentUnit === "celcius") {
-    return {
-      ...api_response,
-      fahrenheit: celsiusToFahrenheit(api_response.celcius),
-    };
-  }
-  return {
-    ...api_response,
-    celcius: farhenheitToCelsius(api_response.fahrenheit),
-  };
-};
+import fetch_weather_from_api from "../utils/weather_helpers";
 
 const weather_api_service = async (city: string, date: string) => {
-  try {
-    const response = await axios.post(
-      `https://staging.v4.api.wander.com/hiring-test/weather`,
-      {
-        city,
-        date,
-      },
-    );
-    if (response.status === 200) {
-      return response_handler(response.data);
-    }
-    throw response.data;
-  } catch (err) {
-    throw new ErrorResponse(
-      "External API error. Please try again later",
-      500,
-      err,
-    );
-  }
+  return fetch_weather_from_api(city, date);
 };
 
 export default {
