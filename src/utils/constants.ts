5aed93c2706c76b09e11a9bd855cfcf8b3d787b8
Ay-slim <ayooluwaadedipe@gmail.com>
2024-09-14 18:36:28 +0000

    App constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-export const DATABASE_ERROR = "Database Error";
-export const RESERVED_ERROR_MESSAGES = [DATABASE_ERROR];
-export const TEMEPERATURE_LAG_PERIOD = 60 * 60 * 1000; // 1 hour
+export const DATABASE_ERROR = "DATABASE ERROR!!: ";
+export const CACHE_EXPIRY = 60 * 60 * 1000; // 1 hour
 export const API_URL = `https://staging.v4.api.wander.com/hiring-test/weather`;
+export const PORT = 8080;
+export const CUSTOM_ERROR_PREFIX = "ERROR: ";
